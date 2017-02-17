ec5d0d50afb772eb2b5750b394157a6810aca21e
vadim <koozyy@bigmir.net>
2017-02-18 01:24:53 +0200

    update project

--- src/task2/2.sql
+++ src/task2/2.sql
@@ -1,25 +1,6 @@
-CREATE TEMPORARY TABLE IF NOT EXISTS temporary_table (
-  id           INT         NOT NULL,
-  project_name VARCHAR(50) NOT NULL,
-  sum          REAL
-);
-
-
-INSERT INTO temporary_table (
-  SELECT
-    projects.id,
-    projects.project_name,
-    sum(developers.salary)
-  FROM project_developers
-    INNER JOIN developers ON project_developers.developer_id = developers.id
-    INNER JOIN projects ON project_developers.project_id = projects.id
-  GROUP BY projects.id
-);
-
-
-SELECT
-  project_name,
-  max(sum)
-FROM temporary_table
-GROUP BY project_name
-LIMIT 1;
+WITH sum_table AS (
+SELECT project_name, sum(salary) as sum FROM project_developers
+INNER JOIN projects ON project_developers.project_id = projects.id
+INNER JOIN developers ON project_developers.developer_id = developers.id
+GROUP BY project_name)
+SELECT sum_table.project_name, max(sum_table.sum) AS max FROM sum_table WHERE sum = (SELECT max(sum_table.sum) FROM sum_table) GROUP BY sum_table.project_name;
